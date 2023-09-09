9f7ab375d5dd8c1a0bd3030ae6c7e7b3a8e84a6d
Rakshana207 <112973484+Rakshana207@users.noreply.github.com>
2023-09-10 00:52:45 +0530

    Add files via upload

---
+++ String_builder.java
@@ -0,0 +1,44 @@
+package special_classes;
+
+public class String_builder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        
+		        StringBuilder sb1=new StringBuilder("I");
+		        System.out.println(sb1);
+		        
+		        sb1.append(" like");
+		        sb1.append(" java");
+		        
+		        System.out.println(sb1);
+		        
+		        StringBuilder sb2=new StringBuilder("learning is fun ");
+		        
+		        sb2.insert(9,"java ");
+		        
+		        System.out.println(sb2);
+		        
+		        sb2.replace(9, 13, "kava");
+		        
+		        System.out.println(sb2);
+		        
+		        sb2.delete(9, 13);
+		        
+		        System.out.println(sb2);
+		        
+		        sb2.deleteCharAt(11);
+		        
+		        System.out.println(sb2);
+		        
+		        sb2.reverse();
+		        System.out.println(sb2);
+		        
+		        
+		        
+		        
+		        
+
+		    }
+
+}
