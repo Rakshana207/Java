473e048e811366cd5b1f487e90334e3de279d9dd
Rakshana207 <112973484+Rakshana207@users.noreply.github.com>
2023-09-12 23:51:01 +0530

    Add files via upload

---
+++ AnimalMain.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class AnimalMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal A=new Animal();
+		Dog D=new Dog();
+		Cat C=new Cat();
+		A.WhoAmI();
+		D.WhoAmI();
+		C.WhoAmI();
+	}
+
+}
