b540bdb1a74688d2671250d7f19a6daedca5a48f
Rakshana207 <112973484+Rakshana207@users.noreply.github.com>
2023-09-15 23:26:00 +0530

    Add files via upload
    Abstraction

---
+++ Mobile_Main.java
@@ -0,0 +1,16 @@
+package abstarction;
+
+public class Mobile_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mobile M;
+		M=new Android();
+		M.doubletap();
+		
+		M=new Ios();
+		M.doubletap();
+		
+	}
+
+}
