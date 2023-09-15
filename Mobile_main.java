4b70cbeb10cd472496a6ab2eee8663f324866b47
Rakshana207 <112973484+Rakshana207@users.noreply.github.com>
2023-09-16 00:18:04 +0530

    Add files via upload
    Interference

---
+++ Mobile_main.java
@@ -0,0 +1,14 @@
+package interference;
+
+public class Mobile_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mobile M;
+		M=new Android();
+		M.doubletap();
+		M=new Ios();
+		M.doubletap();
+	}
+
+}
