e67e3f8b8936a209a4704f4e98a162d55eba2e73
Rakshana207 <112973484+Rakshana207@users.noreply.github.com>
2023-09-20 23:59:52 +0530

    Update Poly_Main.java

--- Poly_Main.java
+++ Poly_Main.java
@@ -5,12 +5,12 @@ public class Poly_Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Triangle t=new Triangle();
-		Rectangle R=new Rectangle();
+		Rectangle r=new Rectangle();
 		
 		t.setbaseandheight(10.0,14.3);
-		R.setbaseandheight(20.5,10.3);
+		r.setbaseandheight(20.5,10.3);
 		t.tri_area();
-		R.rec_area();
+		r.rec_area();
 	}
 
 }
